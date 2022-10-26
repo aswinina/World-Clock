8cf8e7b9fd2513a325f53210fc90ad92b2dfb8bb
aswini <aswinina@users.noreply.github.com>
2022-10-26 19:49:11 +0200

    add link for all city

--- src/index.js
+++ src/index.js
@@ -40,7 +40,9 @@ function updateCity(event) {
   <div class="time">${cityTime.format("h:mm:ss")}<small>${cityTime.format(
     "A"
   )}</small></div>
-  </div>`;
+  </div>
+  <a herf="/"></a>
+  `;
 }
 updateTime();
 setInterval(updateTime, 1000);
